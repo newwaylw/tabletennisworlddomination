1b287c4a957f990640325c48768bd7bd8ad3c9fc
Wei Liu <wei@touchtype-online.com>
2012-08-30 16:32:38 +0100

    added some datastore methods

--- tabletennisworld/domination.py
+++ tabletennisworld/domination.py
@@ -8,25 +8,26 @@ from google.appengine.api import users
 #from google.appengine.ext.webapp.util import run_wsgi_app
 
 class InvasionHistory(db.Model):
-    '''records an invasion '''
-    #id = db.IntegerProperty()
-    invader = db.StringProperty(required=True) #login detail ?
-    countryOfInvader = db.StringProperty(required=True) 
-    defender = db.StringProperty(required=True) #login detail ?
-    countryOfdefender = db.StringProperty(required=True)
-    date = db.DateTimeProperty(auto_now_add=True)
+  '''records an invasion '''
+  #id = db.IntegerProperty()
+  invader = db.StringProperty(required=True) #login detail ?
+  countryOfInvader = db.StringProperty(required=True) 
+  defender = db.StringProperty(required=True) #login detail ?
+  countryOfdefender = db.StringProperty(required=True)
+  date = db.DateTimeProperty(auto_now_add=True)
 
 class Territory(db.Model):
-    '''records the country and who occupies it'''
-    countryName = db.StringProperty(required=True)
-    '''current person'''
-    occupier = db.StringProperty() 
+  '''records the country and who occupies it'''
+  countryName = db.StringProperty(required=True)
+  '''current person'''
+  occupier = db.StringProperty() 
     
 class Challenges(db.Model):
   '''a list of open challenges'''
   invader = db.StringProperty(required=True)
   defender = db.StringProperty(required=True)
   countryOfdefender = db.StringProperty(required=True)
+  is_open = db.BooleanProperty(required=True)
   date = db.DateTimeProperty(auto_now_add=True)
     
 def get_salutation(email):
@@ -34,10 +35,16 @@ def get_salutation(email):
   segment of their email address """
   return email.split("@")[0].replace(".", " ").title() 
 
-def everybodys_name():
-  """ Get a list of everybody's names for reverse name matching """
-  return Territory.all();
-    
+'''sets an open challenge'''  
+def setOpenChallenge(result):
+  challenge = Challenges(result['invader'],result['countryOfInvader'], result['defender'] , result['countryOfdefender'] ,True, datetime.datetime.now() )  
+  challenge.put()
+   
+'''should we remove once the challenge/invasion is finished/expired instead?'''
+def closeChallenge(result):
+  challenge = Challenges(result['invader'],result['countryOfInvader'], result['defender'] , result['countryOfdefender'] ,False, datetime.datetime.now() )  
+  challenge.put()
+  
 def update_result(result):
   """ Takes a series of parameters representing a match and commits the result,
   changing rankings and news feeds where necessary """
@@ -52,27 +59,14 @@ def update_result(result):
           countryOfdefender,
           datetime.datetime.now() )
 
-  #
+  #update territory info
   t_record = Territory.get_by_key_name(countryOfdefender)
   t_record.occupier = invader
-  Territory.put(t_record)  
-  #update territory info
+  t_record.put()  
 
-  db.put([invasionHistory,])
-'''    
-class Greeting(db.Model):
-  """Models an individual Guestbook entry with an author, content, and date."""
-  author = db.StringProperty()
-  content = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
-'''
+  db.put([invasionHistory])
 
 '''
-def guestbook_key(guestbook_name=None):
-  """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-  return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
-'''
-
 class MainPage(webapp2.RequestHandler):
   def get(self):
     self.response.out.write('<html><body>')
@@ -111,47 +105,9 @@ class MainPage(webapp2.RequestHandler):
                           cgi.escape(guestbook_name)))
 
 
-class AppPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.out.write('<html><body>')
-        countries = db.GqlQuery("SELECT * "
-                                 "FROM Invation "
-                                 #"WHERE ANCESTOR IS :1 "
-                                 #"ORDER BY date DESC LIMIT 10"
-                                 )
-        
-        
-'''
-class Guestbook(webapp2.RequestHandler):
-  def post(self):
-    # We set the same parent key on the 'Greeting' to ensure each greeting is in
-    # the same entity group. Queries across the single entity group will be
-    # consistent. However, the write rate to a single entity group should
-    # be limited to ~1/second.
-    guestbook_name = self.request.get('guestbook_name')
-    greeting = Greeting(parent=guestbook_key(guestbook_name))
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user().nickname()
-
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
-
-
-app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/sign', Guestbook)],
-                              debug=True)
-
-
-class Domination(webapp2.RequestHandler):
-  def post(self):
-      
-'''      
-      
-      
 def main():
     run_wsgi_app(app)
-
+'''
 if __name__ == "__main__":
     main()
